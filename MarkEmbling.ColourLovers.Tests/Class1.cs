f0536a23ef889175b60481daa5e8b44da2f4b56a
Mark Embling <mark@markembling.info>
2014-06-23 20:38:54 +0100

    Initial commit of empty project

---
+++ MarkEmbling.ColourLovers.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkEmbling.ColourLovers.Tests
+{
+    public class Class1
+    {
+    }
+}
