01e9685981d132e3d268819d2092766713c3bc89
Mark Embling <mark@markembling.info>
2014-06-25 10:31:05 +0100

    Start work on deserialising colours

---
+++ MarkEmbling.ColourLovers.Tests/ColorDeserializerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MarkEmbling.ColourLovers.Tests {
+    [TestFixture]
+    public class ColorDeserializerTests {
+        private ColorDeserializer _deserialiser;
+
+        [SetUp]
+        public void Setup() {
+            _deserialiser = new ColorDeserializer();
+        }
+
+        [Test]
+        public void Deserialises_single_color_xml_into_list_of_one_color() {
+            using (var file = new FileStream("Data/single_color_1693A5.xml", FileMode.Open)) {
+                var result = _deserialiser.Deserialize(file);
+                Assert.AreEqual(1, result.Count());
+            }
+        }
+
+        [Test]
+        public void Color_has_correct_id() {
+            using (var file = new FileStream("Data/single_color_1693A5.xml", FileMode.Open)) {
+                var result = _deserialiser.Deserialize(file).First();
+                Assert.AreEqual(1086335, result.Id);
+            }
+        }
+
+        [Test]
+        public void Color_has_correct_title() {
+            using (var file = new FileStream("Data/single_color_1693A5.xml", FileMode.Open)) {
+                var result = _deserialiser.Deserialize(file).First();
+                Assert.AreEqual("dutch teal", result.Title);
+            }
+        }
+    }
+}
---
+++ MarkEmbling.ColourLovers/ColorDeserializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace MarkEmbling.ColourLovers {
+    public class ColorDeserializer {
+        public IEnumerable<Color> Deserialize(Stream data) {
+            
+            var doc = XDocument.Load(new XmlTextReader(data));
+            var results = new List<Color>();
+
+            foreach (var element in doc.Root.Elements()) {
+                if (element.Name == "color") {
+                    results.Add(ColorFromElement(element));
+                } else {
+                    // TODO: better exceptions
+                    throw new Exception("NOT A COLOR ELEMENT");
+                }
+            }
+
+            return results;
+        }
+
+        private Color ColorFromElement(XElement element) {
+            var color = new Color();
+            color.Id = int.Parse(element.Element("id").Value);
+            color.Title = element.Element("title").Value;
+            return color;
+        }
+    }
+}
