a61d7f6386f9b26affb49ba512be1f00f041a236
Mark Embling <mark@markembling.info>
2014-06-25 10:58:20 +0100

    Deserialise all colour fields

--- MarkEmbling.ColourLovers/ColorDeserializer.cs
+++ MarkEmbling.ColourLovers/ColorDeserializer.cs
@@ -25,25 +25,48 @@ namespace MarkEmbling.ColourLovers {
         }
 
         private Color ColorFromElement(XElement element) {
-            var color = new Color();
-            color.Id = int.Parse(element.Element("id").Value);
-            color.Title = element.Element("title").Value;
-            color.Username = element.Element("userName").Value;
-            color.NumberOfViews = int.Parse(element.Element("numViews").Value);
-            color.NumberOfVotes = int.Parse(element.Element("numVotes").Value);
-            color.NumberOfComments = int.Parse(element.Element("numComments").Value);
-            color.NumberOfHearts = int.Parse(element.Element("numHearts").Value);
-            color.Rank = int.Parse(element.Element("rank").Value);
-            color.DateCreated = DateTime.Parse(element.Element("dateCreated").Value);
-            color.Hex = element.Element("hex").Value;
-//            color.Rgb = RgbFromElement(element.Element("rgb"));
-//            color.Hsv = HsvFromElement(element.Element("hsv"));
-            color.Description = element.Element("description").Value;
-            color.Url = element.Element("url").Value;
-            color.ImageUrl = element.Element("imageUrl").Value;
-            color.BadgeUrl = element.Element("badgeUrl").Value;
-            color.ApiUrl = element.Element("apiUrl").Value;
+            // ReSharper disable PossibleNullReferenceException
+            var color = new Color {
+                Id = int.Parse(element.Element("id").Value),
+                Title = element.Element("title").Value,
+                Username = element.Element("userName").Value,
+                NumberOfViews = int.Parse(element.Element("numViews").Value),
+                NumberOfVotes = int.Parse(element.Element("numVotes").Value),
+                NumberOfComments = int.Parse(element.Element("numComments").Value),
+                NumberOfHearts = int.Parse(element.Element("numHearts").Value),
+                Rank = int.Parse(element.Element("rank").Value),
+                DateCreated = DateTime.Parse(element.Element("dateCreated").Value),
+                Hex = element.Element("hex").Value,
+                Rgb = RgbFromElement(element.Element("rgb")),
+                Hsv = HsvFromElement(element.Element("hsv")),
+                Description = element.Element("description").Value,
+                Url = element.Element("url").Value,
+                ImageUrl = element.Element("imageUrl").Value,
+                BadgeUrl = element.Element("badgeUrl").Value,
+                ApiUrl = element.Element("apiUrl").Value
+            };
+            // ReSharper restore PossibleNullReferenceException
             return color;
+        }
+
+        private ColorRgb RgbFromElement(XElement element) {
+            // ReSharper disable PossibleNullReferenceException
+            return new ColorRgb {
+                Red = int.Parse(element.Element("red").Value),
+                Green = int.Parse(element.Element("green").Value),
+                Blue = int.Parse(element.Element("blue").Value)
+            };
+            // ReSharper restore PossibleNullReferenceException
+        }
+
+        private ColorHsv HsvFromElement(XElement element) {
+            // ReSharper disable PossibleNullReferenceException
+            return new ColorHsv {
+                Hue = int.Parse(element.Element("hue").Value),
+                Saturation = int.Parse(element.Element("saturation").Value),
+                Value = int.Parse(element.Element("value").Value)
+            };
+            // ReSharper restore PossibleNullReferenceException
         }
     }
 }
